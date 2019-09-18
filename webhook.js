e2395ced90b8050eee78e17fa293cc28621a7ef2
lvlei <lvlei@missfresh.cn>
2019-09-18 15:54:40 +0800

    init

--- webhook.js
+++ webhook.js
@@ -1,6 +1,6 @@
 let http = require('http')
-let server = http.createServer(function (req,res) {
-	if(req.method === 'POST' && req.url=='/webhook'){
+let server = http.createServer(function(req,res){
+	if(req.method == 'POST' && req.url == '/webhook'){
 		res.setHeader('Content-Type','application/json')
 		res.end(JSON.stringify({ok:true}))
 	}else {
