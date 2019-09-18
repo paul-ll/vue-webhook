d68e4999b6d55ca96282977420e2f3c3602262ce
lvlei <lvlei@missfresh.cn>
2019-09-18 16:32:01 +0800

    fix1

--- webhook.js
+++ webhook.js
@@ -1,6 +1,22 @@
 let http = require('http')
+let crypto = require('crypto')
+let {spawn} = require('child_process');
+let SECRET= '441768';
+let sendMail = require('./sendMail');
+function sign(body){
+    return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex');
+}
 let server = http.createServer(function(req,res){
 	if(req.method == 'POST' && req.url == '/webhook'){
+		let buffers = []
+		req.on('data',function(buffer){
+			buffers.push(buffer)
+		})
+		req.on('end',function(buffer){
+		   let body = buffer.concat(buffers);
+		   let event= req.header['x-github-event'];
+
+		})
 		res.setHeader('Content-Type','application/json')
 		res.end(JSON.stringify({ok:true}))
 	}else {
