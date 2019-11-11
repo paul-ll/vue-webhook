bc02081bc2abf0b004e15b4f8a118a79b57d252e
lvlei <lvlei@missfresh.cn>
2019-11-11 18:57:51 +0800

    fix

--- webhook.js
+++ webhook.js
@@ -1,8 +1,8 @@
 let http = require('http')
 let crypto = require('crypto')
-// let {spawn} = require('child_process');
+let {spawn} = require('child_process');
 let SECRET= '441768';
-// let sendMail = require('./sendMail');
+let sendMail = require('./sendMail');
 function sign(body){
     return `sha1=`+crypto.createHmac('sha1',SECRET).update(body).digest('hex');
 }
@@ -23,6 +23,22 @@ let server = http.createServer(function(req,res){
 		})
 		res.setHeader('Content-Type','application/json')
 		res.end(JSON.stringify({ok:true}))
+		 if(event === 'push'){
+        let payload = JSON.parse(body);
+        let child = spawn('sh', [`./${payload.repository.name}.sh`]);
+        let buffers = [];
+        child.stdout.on('data', function (buffer) { buffers.push(buffer)});
+        child.stdout.on('end', function () {
+          let logs = Buffer.concat(buffers).toString();
+          sendMail(`
+            <h1>部署日期: ${new Date()}</h1>
+            <h2>部署人: ${payload.pusher.name}</h2>
+            <h2>部署邮箱: ${payload.pusher.email}</h2>
+            <h2>提交信息: ${payload.head_commit&&payload.head_commit['message']}</h2>
+            <h2>布署日志: ${logs.replace("\r\n",'<br/>')}</h2>
+        `);
+        });
+      }
 	}else {
 		res.end('Not Found')
 	}
